4a23c7fcf5050eb6542739d947f996fff0c93737
Aldo <aldo.choque@tecsup.edu.pe>
2025-08-19 17:58:27 -0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-"use client"
-import Header from "@/components/header"
-import HeroSection from "@/components/hero-section"
-import InnovationSection from "@/components/innovation-section"
-import ExperienceSection from "@/components/experience-section"
-import TestimonialSection from "@/components/testimonial-section"
-import ClientLogosSection from "@/components/client-logos-section"
-import ProductsSection from "@/components/products-section"
-import StatisticsSection from "@/components/statistics-section"
-import FAQSection from "@/components/faq-section"
-import BlogSection from "@/components/blog-section"
-import LocationsSection from "@/components/locations-section"
-import ContactSection from "@/components/contact-section"
-import Footer from "@/components/footer"
+"use client";
+import Header from "@/components/header";
+import HeroSection from "@/components/hero-section";
+import InnovationSection from "@/components/innovation-section";
+import ExperienceSection from "@/components/experience-section";
+import TestimonialSection from "@/components/testimonial-section";
+import ClientLogosSection from "@/components/client-logos-section";
+import ProductsSection from "@/components/products-section";
+import StatisticsSection from "@/components/statistics-section";
+import FAQSection from "@/components/faq-section";
+import BlogSection from "@/components/blog-section";
+import LocationsSection from "@/components/locations-section";
+import ContactSection from "@/components/contact-section";
+import Footer from "@/components/footer";
 
 export default function PlastigesaPage() {
   return (
@@ -30,5 +30,5 @@ export default function PlastigesaPage() {
       <ContactSection />
       <Footer />
     </div>
-  )
+  );
 }
